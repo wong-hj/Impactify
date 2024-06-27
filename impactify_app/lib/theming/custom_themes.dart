bbbde55746fb0662d000682b7cd55adc6787b6d1
WONG HORNG JUN <hj.wong02@gmail.com>
2024-06-27 15:10:20 +0800

    Update

---
+++ impactify_app/lib/theming/custom_themes.dart
@@ -0,0 +1,56 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF006400);
+  static const Color secondary = Color(0xFF66CDAA);
+  static const Color tertiary = Color(0xFF98FB98);
+  static const Color placeholder = Color(0xFF696969);
+  static const Color background = Color(0xFFF5FFFF);
+
+
+}
+
+class AppTextStyles {
+  static final TextStyle displayLarge = GoogleFonts.modak(
+    fontSize: 32.0,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary,
+  );
+
+  static const TextStyle bodyText1 = TextStyle(
+    fontSize: 16.0,
+    color: AppColors.secondary,
+  );
+
+  // Add more text styles as needed
+}
+
+class CustomTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primaryColor: AppColors.primary,
+      hintColor: AppColors.secondary,
+      scaffoldBackgroundColor: AppColors.background,
+      textTheme: const TextTheme(
+        displayLarge: TextStyle(color: AppColors.primary),
+        bodyLarge: TextStyle(color: AppColors.secondary),
+      ),
+      // Define other theme properties as needed
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: AppColors.primary,
+      hintColor: AppColors.secondary,
+      scaffoldBackgroundColor: AppColors.background,
+      textTheme: const TextTheme(
+        displayLarge: TextStyle(color: AppColors.primary),
+        bodyLarge: TextStyle(color: AppColors.secondary),
+      ),
+      // Define other theme properties as needed
+    );
+  }
+}
