81636e69f687cd94f6b67317829ed00f82b53447
WONG HORNG JUN <hj.wong02@gmail.com>
2024-07-09 19:09:42 +0800

    Still working on google map, backup

--- impactify_app/lib/screens/user/eventDetails.dart
+++ impactify_app/lib/screens/user/eventDetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:impactify_app/constants/sdglogo.dart';
@@ -20,11 +21,32 @@ class EventDetail extends StatefulWidget {
 
 class _EventDetailState extends State<EventDetail> {
   late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  LatLng? _center;
+  Marker? _marker;
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  Future<Map<String, dynamic>> _fetchEventAndLocation(String eventID) async {
+    final eventProvider = Provider.of<EventProvider>(context, listen: false);
+    Event event = await eventProvider.getEventByID(eventID);
+    List<Location> locations = await locationFromAddress(event.location);
+
+    LatLng center = LatLng(locations.first.latitude, locations.first.longitude);
+    Marker marker = Marker(
+      markerId: MarkerId(event.location),
+      position: center,
+      infoWindow: InfoWindow(
+        title: event.location,
+      ),
+    );
+
+    return {
+      'event': event,
+      'center': center,
+      'marker': marker,
+    };
   }
 
   @override
@@ -38,8 +60,8 @@ class _EventDetailState extends State<EventDetail> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: FutureBuilder<Event>(
-        future: eventProvider.getEventByID(eventID),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: _fetchEventAndLocation(eventID),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CustomLoading(text: 'Loading...'));
@@ -48,17 +70,23 @@ class _EventDetailState extends State<EventDetail> {
           } else if (!snapshot.hasData) {
             return Center(child: Text('Event not found'));
           } else {
-            Event event = snapshot.data!;
+            Event event = snapshot.data!['event'];
+            LatLng center = snapshot.data!['center'];
+            Marker marker = snapshot.data!['marker'];
+
             return CustomDetailScreen(
               imageUrl: event.eventImage,
               type: 'EVENT',
               title: event.title,
               hoster: event.organizer,
               location: event.location,
-              date: event.hostDate.toString(),
+              hostDate: event.hostDate,
               aboutDescription: event.description,
+              impointsAdd: event.impointsAdd,
+              marker: marker,
               onMapCreated: _onMapCreated,
-              center: _center,
+              center: center,
+              sdg: event.sdg,
             );
           }
         },
--- impactify_app/lib/widgets/custom_details.dart
+++ impactify_app/lib/widgets/custom_details.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -5,19 +6,21 @@ import 'package:impactify_app/constants/sdglogo.dart';
 import 'package:impactify_app/theming/custom_themes.dart';
 import 'package:impactify_app/widgets/custom_buttons.dart';
 import 'package:impactify_app/widgets/custom_text.dart';
+import 'package:intl/intl.dart';
 
 class CustomDetailScreen extends StatelessWidget {
-
   final String imageUrl;
   final String type;
   final String title;
   final String hoster;
   final String location;
-  final String date;
+  final Timestamp hostDate;
   final String aboutDescription;
-  final String? sdgDescription;
+  final int impointsAdd;
+  final String? sdg;
+  final Marker? marker;
   final Function(GoogleMapController) onMapCreated;
-  final LatLng center;
+  final LatLng? center;
 
   const CustomDetailScreen({
     required this.imageUrl,
@@ -25,148 +28,149 @@ class CustomDetailScreen extends StatelessWidget {
     required this.title,
     required this.hoster,
     required this.location,
-    required this.date,
+    required this.hostDate,
     required this.aboutDescription,
+    required this.impointsAdd,
     required this.onMapCreated,
-    required this.center,
-    this.sdgDescription,
+    this.marker,
+    this.center,
+    this.sdg,
     Key? key,
   }) : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context) {
+    DateTime date = hostDate.toDate();
+    String formattedDate =
+        DateFormat('dd MMMM yyyy, HH:mm').format(date).toUpperCase();
+
     return SingleChildScrollView(
-        child: Column(
-          children: [
-            // Picture
-            Stack(
-              children: [
-                Container(
-                  height: 300,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(imageUrl),
-                      fit: BoxFit.cover,
-                    ),
+      child: Column(
+        children: [
+          // Picture
+          Stack(
+            children: [
+              Container(
+                height: 300,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(imageUrl),
+                    fit: BoxFit.cover,
                   ),
-                ),
-                Positioned(
-                  top: 40,
-                  right: 30,
-                  child: Material(
-                    elevation: 10,
-                    child: Container(
-                      width: 60,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(SDG4),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            // Content
-            Container(
-              decoration: BoxDecoration(
-                color: AppColors.background,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
                 ),
               ),
-              padding: EdgeInsets.all(20),
-              transform: Matrix4.translationValues(0.0, -30.0, 0.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        type,
-                        style: GoogleFonts.nunito(
-                            fontSize: 12, color: AppColors.primary),
+              Positioned(
+                top: 40,
+                right: 30,
+                child: Material(
+                  elevation: 10,
+                  child: Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            "https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-${sdg}.jpg"),
+                        fit: BoxFit.cover,
                       ),
-                      Spacer(),
-                      Icon(Icons.bookmark_border),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // Content
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.background,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+            ),
+            padding: EdgeInsets.all(20),
+            transform: Matrix4.translationValues(0.0, -30.0, 0.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      type,
+                      style: GoogleFonts.nunito(
+                          fontSize: 12, color: AppColors.primary),
+                    ),
+                    Spacer(),
+                    Icon(Icons.bookmark_border),
+                  ],
+                ),
+                SizedBox(height: 5),
+                Text(
+                  title,
+                  style: GoogleFonts.nunito(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Hosted by ',
+                        style: GoogleFonts.nunito(
+                            fontSize: 12, color: AppColors.placeholder),
+                      ),
+                      TextSpan(
+                        text: hoster,
+                        style: GoogleFonts.nunito(
+                            fontSize: 12,
+                            color: AppColors.primary,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ],
                   ),
-                  SizedBox(height: 5),
-                  Text(
-                    title,
-                    style: GoogleFonts.nunito(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Text.rich(
-                    TextSpan(
-                      children: [
-                        TextSpan(
-                          text: 'Hosted by ',
-                          style: GoogleFonts.nunito(
-                              fontSize: 12, color: AppColors.placeholder),
-                        ),
-                        TextSpan(
-                          text: hoster,
-                          style: GoogleFonts.nunito(
-                              fontSize: 12,
-                              color: AppColors.primary,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  CustomIconText(
-                      text: location,
-                      icon: Icons.location_on, 
-                      size: 12),
-                  SizedBox(height: 8),
-                  CustomIconText(
-                      text: date, icon: Icons.calendar_month, size: 12),
-                  SizedBox(height: 8),
-                  CircleAvatar(
-                    radius: 15,
-                    backgroundImage:
-                        NetworkImage('https://via.placeholder.com/40'),
-                  ),
-                  SizedBox(height: 16),
+                ),
+                SizedBox(height: 8),
+                CustomIconText(
+                    text: location, icon: Icons.location_on, size: 12),
+                SizedBox(height: 8),
+                CustomIconText(
+                    text: formattedDate, icon: Icons.calendar_month, size: 12),
+                SizedBox(height: 8),
+                CircleAvatar(
+                  radius: 15,
+                  backgroundImage:
+                      NetworkImage('https://via.placeholder.com/40'),
+                ),
+                SizedBox(height: 16),
+                CustomLargeIconText(
+                    icon: Icons.info_outlined, text: 'About this Event'),
+                SizedBox(height: 8),
+                Text(
+                  "${aboutDescription}\n\n**Participation adds ${impointsAdd} Impoints!",
+                  textAlign: TextAlign.justify,
+                  style: GoogleFonts.poppins(
+                      fontSize: 12, color: AppColors.placeholder),
+                ),
+                SizedBox(height: 16),
+                if (type != "SPEECH") ...[
                   CustomLargeIconText(
-                      icon: Icons.info_outlined, text: 'About this Event'),
-
+                      icon: Icons.flag_outlined,
+                      text: 'Sustainable Development Goals'),
                   SizedBox(height: 8),
                   Text(
-                    aboutDescription,
+                    'This event tackles SDG ${sdg}',
                     textAlign: TextAlign.justify,
                     style: GoogleFonts.poppins(
                         fontSize: 12, color: AppColors.placeholder),
                   ),
                   SizedBox(height: 16),
-                  if(type != "SPEECH") ...[
-                    CustomLargeIconText(
-                        icon: Icons.flag_outlined,
-                        text: 'Sustainable Development Goals'),
-                    SizedBox(height: 8),
-                    Text(
-                      'This event tackles SDG xxx',
-                      textAlign: TextAlign.justify,
-                      style: GoogleFonts.poppins(
-                          fontSize: 12, color: AppColors.placeholder),
-                    ),
-                    SizedBox(height: 16),
-                  ],
-                  
-                  CustomLargeIconText(
-                      icon: Icons.explore_outlined, text: 'Location'),
-
-                  SizedBox(height: 8),
+                ],
+                CustomLargeIconText(
+                    icon: Icons.explore_outlined, text: 'Location'),
+                SizedBox(height: 8),
+                if (center != null && marker != null)
                   Container(
                     height: 200,
                     decoration: BoxDecoration(
@@ -178,21 +182,20 @@ class CustomDetailScreen extends StatelessWidget {
                       child: GoogleMap(
                         onMapCreated: onMapCreated,
                         initialCameraPosition: CameraPosition(
-                          target: center,
+                          target: center!,
                           zoom: 11.0,
                         ),
+                        markers: {marker!},
                       ),
                     ),
                   ),
-
-                  SizedBox(height: 16),
-                  CustomPrimaryButton(onPressed: () {}, text: "I'm In!"),
-                  
-                ],
-              ),
+                SizedBox(height: 16),
+                CustomPrimaryButton(onPressed: () {}, text: "I'm In!"),
+              ],
             ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
